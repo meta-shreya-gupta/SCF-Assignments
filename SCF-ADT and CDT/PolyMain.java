d67b75e87bbd4bc2f03ff5374c9049bfa196efdd
Shreya Gupta <shreya.gupta@metacube.com>
2025-03-16 20:38:56 +0530

    comments added in polynomial

--- SCF-ADT and CDT/PolyMain.java
+++ SCF-ADT and CDT/PolyMain.java
@@ -7,10 +7,10 @@ import java.util.HashMap;
  * class to initialise terms of the polynomial
  */
 final class Term{
-    private final int coefficient;
+    private final int coefficient;                  //coefficient and exponent of each term in a polynomial
     private final int exponent;
 
-    public Term(int coefficient , int exponent){
+    public Term(int coefficient , int exponent){    //construction for coefficient and exponent
         this.coefficient = coefficient;
         this.exponent = exponent;
     }
@@ -19,7 +19,7 @@ final class Term{
      * getter method to get coefficient of the polynomial . cannot be 0
      * @return  coefficient of the term of the polynomial
      */
-    public int getCoefficient(){
+    public int getCoefficient(){                        //getter method to get coefficient
         return coefficient;
     }
 
@@ -27,7 +27,7 @@ final class Term{
      * getter method to get the exponent of the term user has entered
      * @return  exponent of the term of the polynomial
      */
-    public int getExponent(){
+    public int getExponent(){                           //getter method to get exponent
         return exponent;
     }
 }
@@ -36,7 +36,7 @@ final class Term{
  * class to create polynomials as an array of Terms
  */
 final class Poly{
-    private final Term[] poly;
+    private final Term[] poly;                          //a polynomial is an array of terms
 
     public Poly(Term[] poly){
         this.poly = poly;
@@ -45,7 +45,7 @@ final class Poly{
      * It gives polynomial as an array of terms
      * @return  array of terms
      */
-    public Term[] getTerms(){
+    public Term[] getTerms(){                           //getter method to get polynomial as an array of terms
         return poly;
     }
 
@@ -54,10 +54,10 @@ final class Poly{
      * @param value on which polynomial is to be evaluated
      * @return computed value of the polynomial
      */
-    public float evaluate(float value){
+    public float evaluate(float value){                 //evaluates polynomial on a specified value
         float computedValue = 0;
-        for(int index = 0 ; index < poly.length ; index ++){
-            computedValue += poly[index].getCoefficient() * Math.pow(value , poly[index].getExponent());
+        for(int index = 0 ; index < poly.length ; index ++){                //iterated through terms of polynomial
+            computedValue += poly[index].getCoefficient() * Math.pow(value , poly[index].getExponent());    //computes value for each term
         }
         return computedValue;
     }
@@ -66,10 +66,10 @@ final class Poly{
      * finds the maximum exponent of the polynomial
      * @return maximum value of the exponent in a polynomial
      */
-    public int getDegree(){
+    public int getDegree(){                             //finds maximum exponent in a polynomial
         int degree = 0;
-        for(int index = 0 ; index < poly.length ; index ++){
-            if(poly[index].getExponent() > degree){
+        for(int index = 0 ; index < poly.length ; index ++){        //iterates through all terms
+            if(poly[index].getExponent() > degree){                 //finds for maximum exponent
                 degree = poly[index].getExponent();
             }
         }
@@ -83,26 +83,26 @@ final class Poly{
      * @return polynomial after adding polynomial p1 and p2 . rule-Like terms are added
      */
     public static Poly addPoly(Poly p1 , Poly p2){
-        int maxSize = p1.poly.length + p2.poly.length;
-        Term[] addResult = new Term[maxSize];
+        int maxSize = p1.poly.length + p2.poly.length;                      //assumes maximum terms to be sum of terms of two polynomials
+        Term[] addResult = new Term[maxSize];                               //terms array of the resultant polynomial
         int resultIndex = 0;
-        for(int indexOfp1 = 0 ; indexOfp1 < p1.poly.length ; indexOfp1 ++){
+        for(int indexOfp1 = 0 ; indexOfp1 < p1.poly.length ; indexOfp1 ++){  //iterates to first polynomial
             boolean found = false;
-            for(int indexOfp2 = 0 ; indexOfp2 < p2.poly.length ; indexOfp2 ++){
-                if(p1.poly[indexOfp1].getExponent() == p2.poly[indexOfp2].getExponent()){
+            for(int indexOfp2 = 0 ; indexOfp2 < p2.poly.length ; indexOfp2 ++){  //iterates to second polynomial
+                if(p1.poly[indexOfp1].getExponent() == p2.poly[indexOfp2].getExponent()){       //if finds exponent to be same then add the coefficient
                     int sumOfCoefficient = p1.poly[indexOfp1].getCoefficient() + p2.poly[indexOfp2].getCoefficient();
-                    if(sumOfCoefficient != 0){
+                    if(sumOfCoefficient != 0){                                                  //if sum is zero then add it to the resultant polynomial
                         addResult[resultIndex++] = new Term(sumOfCoefficient, p1.poly[indexOfp1].getExponent());
                     }
                     found = true;
                     break;
                 }
             }
-            if(! found){
+            if(! found){                                                                //if do not find same exponent terms then add the whole term
                 addResult[resultIndex++] = p1.poly[indexOfp1];
             }
         }
-        for(int indexOfp2 = 0 ; indexOfp2 < p2.poly.length ; indexOfp2 ++){
+        for(int indexOfp2 = 0 ; indexOfp2 < p2.poly.length ; indexOfp2 ++){             //checks for any term that is not in first polynomial
             boolean found = false ;
             for(int indexOfp1 = 0 ; indexOfp1 < p1.poly.length ; indexOfp1++){
                 if(p2.poly[indexOfp2].getExponent() == p1.poly[indexOfp1].getExponent()){
@@ -110,11 +110,11 @@ final class Poly{
                     break;
                 }
             }
-            if(! found){
+            if(! found){                                                              //adds the terms that is not in first polynomial
                 addResult[resultIndex++] = p2.poly[indexOfp2];
             }
         }
-        Term[] finalResult = new Term[resultIndex];
+        Term[] finalResult = new Term[resultIndex];                             //assigns all terms to the resultant polynomila according to the size
         System.arraycopy(addResult, 0, finalResult, 0, resultIndex);
         return new Poly(finalResult);
     }
@@ -126,17 +126,17 @@ final class Poly{
      * @return polynomial after multiplicating both the polynomials
      */
     public static Poly multiplyPoly(Poly p1 , Poly p2){
-        Map<Integer,Integer> resultMap = new HashMap<>();
-        for(int indexOfp1 = 0 ; indexOfp1 < p1.poly.length ; indexOfp1++){
+        Map<Integer,Integer> resultMap = new HashMap<>();                   //map data structure to keep exponents of resultant polynomial
+        for(int indexOfp1 = 0 ; indexOfp1 < p1.poly.length ; indexOfp1++){   //iterates from each term of first polynomial to each term of second polynomial
             for(int indexOfp2 = 0 ; indexOfp2 < p2.poly.length ; indexOfp2++){
-                int newCoefficient = p1.poly[indexOfp1].getCoefficient() * p2.poly[indexOfp2].getCoefficient();
+                int newCoefficient = p1.poly[indexOfp1].getCoefficient() * p2.poly[indexOfp2].getCoefficient();     //finds new exponent and new coefficient
                 int newExponent = p1.poly[indexOfp1].getExponent() + p2.poly[indexOfp2].getExponent();
-                resultMap.put(newExponent,resultMap.getOrDefault(newExponent,0)+newCoefficient);
+                resultMap.put(newExponent,resultMap.getOrDefault(newExponent,0)+newCoefficient);        //adds each new exponent with new coefficients (if already exist then add to value else add both)
             }
         }
-        Term[] multiplyResult = new Term[resultMap.size()];
+        Term[] multiplyResult = new Term[resultMap.size()];                 //new polynomial after multiplying
         int index = 0;
-        for(Map.Entry<Integer,Integer> entry : resultMap.entrySet()){
+        for(Map.Entry<Integer,Integer> entry : resultMap.entrySet()){       //assigning each term of map to term of polynomial
             if(entry.getValue() != 0){
                 multiplyResult[index++] = new Term(entry.getValue(),entry.getKey());
             }
@@ -147,9 +147,9 @@ final class Poly{
      * helper method to print the polynomial
      * @param input polynomial which we wnat to print
      */
-    public static void printPoly(Poly input){
+    public static void printPoly(Poly input){           
         for(Term term : input.poly){
-            System.out.print(term.getCoefficient() +"x^"+term.getExponent()+" ");
+            System.out.print(term.getCoefficient() +"x^"+term.getExponent()+" ");           //prints the polynomial in terms of 'x' 
         }
         System.out.println();
     }
@@ -159,10 +159,10 @@ final class Poly{
 public class PolyMain {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter Number of terms of 1st polynomial");
+        System.out.println("Enter Number of terms of 1st polynomial");      //asking user for number of terms in first polynomial
         int countTerm = sc.nextInt();
-        Term[] terms = new Term[countTerm];
-        for(int index = 0 ; index < countTerm ; index ++){
+        Term[] terms = new Term[countTerm];                                   //first polynomial as an array of terms
+        for(int index = 0 ; index < countTerm ; index ++){                    //asking user coefficient and exponent of each term
             System.out.println("Enter coefficient");
             int coefficient = sc.nextInt();
             System.out.println("Enter exponent");
@@ -170,20 +170,20 @@ public class PolyMain {
             terms[index] = new Term(coefficient,exponent);
         }
         Poly poly1 = new Poly(terms);
-        System.out.println("Enter value of variable at which polynomial is to be evaluated");
+        System.out.println("Enter value of variable at which polynomial is to be evaluated");   //asking the float value at which the user wants to evaluate the polynomial
         float value =sc.nextFloat();
         float evaluateResult = poly1.evaluate(value);
         System.out.println("Evaluation at x = "  + evaluateResult);
-        System.out.println("Degree = " + poly1.getDegree());
-        System.out.println("Enter the operation you want to perfrom : \n1. Add \n2. Multiply \n3.Exit");
+        System.out.println("Degree = " + poly1.getDegree());                                    //printing degree of the polynomial
+        System.out.println("Enter the operation you want to perfrom : \n1. Add \n2. Multiply \n3.Exit");    //asking user the operation he wants to perform
         int choice = sc.nextInt();
         int countTerm2 = 0;
         switch(choice){
             case 1 :
-                System.out.println("Enter Number of terms of 2nd polynomial");
-                countTerm2 = sc.nextInt();
+                System.out.println("Enter Number of terms of 2nd polynomial");                  
+                countTerm2 = sc.nextInt();                                            //asking user to enter number of terms of second polynomial
                 Term[] terms2 = new Term[countTerm2];
-                for(int index = 0 ; index < countTerm2 ; index ++){
+                for(int index = 0 ; index < countTerm2 ; index ++){                   //entering coefficient and exponent of each term of second polynomial
                     System.out.println("Enter coefficient");
                     int coefficient = sc.nextInt();
                     System.out.println("Enter exponent");
@@ -191,14 +191,14 @@ public class PolyMain {
                     terms2[index] = new Term(coefficient,exponent);
                 }
                 Poly poly2 = new Poly(terms2);
-                Poly resultPolyAdd = Poly.addPoly(poly1 , poly2);
-                Poly.printPoly(resultPolyAdd);
+                Poly resultPolyAdd = Poly.addPoly(poly1 , poly2);           //performing addition operation of two polynomials
+                Poly.printPoly(resultPolyAdd);                              //printing resultant polynomial
                 break;
             case 2 :
-                System.out.println("Enter Number of terms of 2nd polynomial");
+                System.out.println("Enter Number of terms of 2nd polynomial");  //asking user to enter number of terms of second polynomial
                 countTerm2 = sc.nextInt();
                 Term[] terms2New = new Term[countTerm2];
-                for(int index = 0 ; index < countTerm2 ; index ++){
+                for(int index = 0 ; index < countTerm2 ; index ++){             //entering coefficient and exponent of each term of second polynomial
                     System.out.println("Enter coefficient");
                     int coefficient = sc.nextInt();
                     System.out.println("Enter exponent");
@@ -206,16 +206,15 @@ public class PolyMain {
                     terms2New[index] = new Term(coefficient,exponent);
                 }
                 Poly poly2New = new Poly(terms2New);
-                Poly resultPolyMultiply = Poly.multiplyPoly(poly1 , poly2New);
-                Poly.printPoly(resultPolyMultiply);
+                Poly resultPolyMultiply = Poly.multiplyPoly(poly1 , poly2New);      //performing multiplication operation of two polynomials
+                Poly.printPoly(resultPolyMultiply);                                 //printing resultant polynomial
                 break;
             case 3 :
-                sc.close();
-                System.exit(0);
+                sc.close();                                         //closing scanner class
+                System.exit(0);                             //terminating the program
             default :
                 System.out.println("Invalid choice entered");
                 
         }
-        sc.close();
     }
 }
