117f2a52e9a5c5f9522ed975df40fcd107b56b4a
Shreya Gupta <shreya.gupta@metacube.com>
2025-03-19 22:00:06 +0530

    factory concept implemented

---
+++ SCF_extensibiltyAndFactory/Shape/ShapeFactory.java
@@ -0,0 +1,31 @@
+package Shape;
+import java.util.List;
+
+import Shape.Shape.ShapeType;
+
+
+public class ShapeFactory {
+    
+    public static Shape createShape(ShapeType type, Point origin , List<Integer> dimensions){
+        Shape shape = null;
+        switch(type){
+            case Rectangle :
+                shape = new Rectangle(origin , dimensions);
+                break;
+            case Square :
+                shape = new Square(origin , dimensions);
+                break;
+            case Circle :
+                shape = new Circle(origin , dimensions);
+                break;
+            case Triangle :
+                shape = new Triangle(origin , dimensions);
+                break;
+            case RegularPolygon :
+                shape = new RegularPolygon(origin ,dimensions);
+            default :
+                System.out.println("Cannot create shape");
+        }
+        return shape;
+    }
+}
