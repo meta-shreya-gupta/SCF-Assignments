b421bc7daf13291e2fb32b923254c24439dae855
Shreya Gupta <shreya.gupta@metacube.com>
2025-03-22 14:12:22 +0530

    test cases added

--- SCF-Using invariant/MatricesTest.java
+++ SCF-Using invariant/MatricesTest.java
@@ -3,49 +3,85 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertArrayEquals;
 public class MatricesTest {
     @Test
-    void transposeOfMatrixTest(){
-        int[][] array1 = {{0,0,0,0,9,0},{0,8,0,0,0,0},{4,0,0,2,0,0},{0,0,0,0,0,5},{0,0,2,0,0,0}};
-        MatricesOperation input1 = new MatricesOperation(5, 6, array1);
-        MatricesOperation output1 = input1.transposeOfMatrix();
-        int[][] actualOutput = output1.getArray();
-        int[][] expectedOutput = {{0,0,4,0,0},{0,8,0,0,0},{0,0,0,0,2},{0,0,2,0,0},{9,0,0,0,0},{0,0,0,5,0}};
-        assertArrayEquals(expectedOutput,actualOutput);
+    public void testTranspose(){
+        int[][] matrix ={
+            {1,0,2},
+            {0,3,0},
+            {4,0,5}
+        };
+        MatricesOperation matricesOperation = new MatricesOperation(3, 3, matrix);
+        MatricesOperation transpose = matricesOperation.transposeOfMatrix();
+        int[][] expected = {
+            {1,0,4},
+            {0,3,0},
+            {2,0,5}
+        };
+        assertArrayEquals(expected, transpose.toMatrix(transpose.sparseArray));
+    }
+    @Test
+    public void isSymmetricalTest_Positive(){
+        int[][] matrix1 = {
+            {1,0,2},
+            {0,3,0},
+            {2,0,4}
+        };
+        MatricesOperation input1 = new MatricesOperation(3, 3, matrix1);
+        assertEquals(true, input1.isSymmetrical());
     }
 
     @Test
-    void isSymmetricalTest(){
-        int[][] array1 = {{0,0,0,0,9,0},{0,8,0,0,0,0},{4,0,0,2,0,0},{0,0,0,0,0,5},{0,0,2,0,0,0}};
-        MatricesOperation input1 = new MatricesOperation(5, 6, array1);
-        boolean actualOutput = input1.isSymmetrical();
-        boolean expectedOutput = false;
-        assertEquals(expectedOutput,actualOutput);
+    public void isSymmetricalTest_Negative(){
+        int[][] matrix1 = {
+            {1,0,7},
+            {0,0,0},
+            {1,0,4}
+        };
+        MatricesOperation input1 = new MatricesOperation(3, 3, matrix1);
+        assertEquals(false, input1.isSymmetrical());
+    }
+    @Test
+    public void addMatricesTest(){
+        int[][] matrix1 = {
+            {1,0,2},
+            {0,3,0},
+            {2,0,4}
+        };
+        int[][] matrix2 = {
+            {2,0,1},
+            {0,4,0},
+            {3,0,2}
+        };
+        MatricesOperation input1 = new MatricesOperation(3, 3, matrix1);
+        MatricesOperation input2 = new MatricesOperation(3, 3, matrix2);
+        MatricesOperation sum = input1.addMatrices(input2);
+        int[][] expected = {
+            {3,0,3},
+            {0,7,0},
+            {5,0,6}
+        };
+        assertArrayEquals(expected, sum.toMatrix(sum.sparseArray));
     }
 
     @Test
-    void addMatricesTest(){
-        int[][] array1 = {{1,2,3},{1,2,3},{1,2,3}};
-        MatricesOperation input1 = new MatricesOperation(3, 3, array1);
-        int[][] array2 = {{4,5,6},{4,5,6},{4,5,6}};
-        MatricesOperation input2 = new MatricesOperation(3, 3, array2);
-        MatricesOperation output = input1.addMatrices(input2);
-        int[][] actualOutput = output.getArray();
-        int[][] output1 = {{5,7,9},{5,7,9},{5,7,9}};
-        MatricesOperation expectedArray = new MatricesOperation(3, 3, output1);
-        int[][] expectedOutput = expectedArray.getArray();
-        assertArrayEquals(expectedOutput,actualOutput);
-    }
-
-    @Test
-    void multiplyMatricesTest(){
-        int[][] array1 = {{1,2,3},{1,2,3},{1,2,3}};
-        MatricesOperation input1 = new MatricesOperation(3, 3, array1);
-        int[][] array2 = {{4,5,6},{4,5,6},{4,5,6}};
-        MatricesOperation input2 = new MatricesOperation(3, 3, array2);
-        MatricesOperation output = input1.multiplyMatrices(input2);
-        int[][] actualOutput = output.getArray();
-        int[][] output1 = {{24,30,36},{24,30,36},{24,30,36}};
-        MatricesOperation expectedArray = new MatricesOperation(3, 3, output1);
-        int[][] expectedOutput = expectedArray.getArray();
-        assertArrayEquals(expectedOutput,actualOutput);
+    public void multiplyMatricesTest(){
+        int[][] matrix1 = {
+            {1,0,2},
+            {0,3,0},
+            {4,0,5}
+        };
+        int[][] matrix2 = {
+            {2,0,1},
+            {0,4,0},
+            {3,0,2}
+        };
+        MatricesOperation input1 = new MatricesOperation(3, 3, matrix1);
+        MatricesOperation input2 = new MatricesOperation(3, 3, matrix2);
+        MatricesOperation product = input1.multiplyMatrices(input2);
+        int[][] expected = {
+            {8,0,5},
+            {0,12,0},
+            {23,0,14}
+        };
+        assertArrayEquals(expected, product.toMatrix(product.sparseArray));
     }
 }
